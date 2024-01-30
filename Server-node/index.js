14f838dd08b0bf3f6846c3bba6589666ae8999f1
Sharon Martin <the.painterly.girl@gmail.com>
2024-01-30 17:35:40 -0600

    Small Changes

---
+++ Server-node/index.js
@@ -0,0 +1,22 @@
+
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+
+
+
+const cowsay= require('cowsay');
+//console.log (cowsay.say9{text: "I am a module"})
